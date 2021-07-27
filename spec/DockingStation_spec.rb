ca287c296be3cfeb0d87cac0d5d5c705fe36fa8f
edpackard <edpackard81@gmail.com>
2021-07-27 16:46:14 +0100

    Added tests for bike in dockingstation_spec.rb

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -5,4 +5,15 @@ describe DockingStation do
     describe "Test if DockingStation responds to release_bike" do
     it { is_expected.to respond_to(:release_bike) }
     end
+
+     it "Tests that DockingStation.release_bike gets a bike" do
+      station = DockingStation.new
+      expect(station.release_bike).to eq Bike.new
+    end
+
+    it "Released bike responds to 'working?'" do
+      bike = (DockingStation.new).release_bike
+      expect(bike.working?).to eq !nil
+    end
+
 end
