051fa0a7965c952149169efc0ef9fa2e21038e0c
Hotu Nga Morehu Elkington <hotungamorehuelkington@ma388.home>
2021-07-27 16:26:00 +0100

    Bike RSpec and Class added

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,6 +1,7 @@
 require "DockingStation"
 
 describe DockingStation do
+    
     describe "Test if DockingStation responds to release_bike" do
     it { is_expected.to respond_to(:release_bike) }
     end
