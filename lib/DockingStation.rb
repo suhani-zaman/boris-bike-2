12320446278c56a3d3d99176330b12bad08288da
Suhani Zaman <suhanibadiz9@gmail.com>
2021-07-29 22:48:54 +0400

    updated till 14 with 20 bikes

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -9,11 +9,16 @@ class DockingStation
 
 	def release_bike
 		raise "no bikes to release" if check == false
-  	bike = Bike.new
+		if check == true
+			bike = @dock.pop()
+		end
 	end
 
-	def dock_bike
-		@dock.push(Bike.new)
+	def dock
+		raise "Dock station full" if @dock.count >= 20
+		if @dock.count < 20
+			@dock << Bike.new
+		end
 	end
 	def check
 		if @dock.count > 0
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,13 +1,13 @@
 require "DockingStation"
 
 describe DockingStation do
-
+    
     describe "Test if DockingStation responds to release_bike" do
       it { is_expected.to respond_to(:release_bike) }
     end
 
     describe "Tests that reponds to dock bike" do
-      it { is_expected.to respond_to(:dock_bike) }
+      it { is_expected.to respond_to(:dock) }
     end
 
     it "Tests that DockingStation.release_bike gets a bike and then if it's working" do
@@ -18,5 +18,12 @@ describe DockingStation do
 
     it "checks if bike is available otherwise raises an error" do
       expect { subject.release_bike }.to raise_error "no bikes to release"
+    end
+    it "check if dock station exceeds the limit provided" do
+#      for i in 1..21 do
+#        subject.dock_bike
+#      end
+      20.times { subject.dock}
+      expect { subject.dock}.to raise_error "Dock station full"
     end
 end
